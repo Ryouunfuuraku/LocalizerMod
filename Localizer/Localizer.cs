41b457f3f7b62b809e19a1840bee395824966b57
AxeelAnder <1491773534@qq.com>
2018-10-11 16:38:59 +0800

    Setup projects

---
+++ Localizer/Localizer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Localizer
+{
+    public class Localizer : Mod
+    {
+		public override void Load()
+		{
+
+		}
+
+		public override void PostSetupContent()
+		{
+
+		}
+
+	}
+}
