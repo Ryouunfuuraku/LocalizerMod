4023e6690d426a1d95d5fa5c9619edb18cff7f75
AxeelAnder <1491773534@qq.com>
2018-10-12 09:29:47 +0800

    want to add hook

---
+++ Localizer/Patches.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Harmony;
+
+namespace Localizer
+{
+	public class Patches
+	{
+		[HarmonyPatch(typeof(NPCLoader))]
+		[HarmonyPatch("SetChatButtons")]
+		[HarmonyPatch(new Type[] { typeof(string), typeof(string)})]
+		class AddChatButtonHook
+		{
+		}
+	}
+}
