779250269f976cee50f9dc48da0feac263b94da4
AxeelAnder <1491773534@qq.com>
2018-10-19 20:37:09 +0800

    added export button

--- Localizer/UI/UIModListItem.cs
+++ Localizer/UI/UIModListItem.cs
@@ -4,12 +4,13 @@ using System.IO;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria.GameContent.UI.Elements;
+using Terraria;
 using Terraria.Graphics;
 using Terraria.UI;
+using Terraria.ModLoader;
 using System.Linq;
 using Terraria.Localization;
 using System.Reflection;
-using Terraria.ModLoader;
 
 namespace Localizer.UI
 {
@@ -17,12 +18,9 @@ namespace Localizer.UI
 	{
 		private readonly Mod mod;
 		private readonly Texture2D dividerTexture;
-		private int modIconAdjust;
 		private readonly Texture2D innerPanelTexture;
 		private readonly UIText modName;
 		private readonly UITextPanel<string> button2;
-		private UIImage modIcon;
-		private bool loaded;
 
 		public UIModListItem(Mod mod)
 		{
@@ -36,7 +34,7 @@ namespace Localizer.UI
 
 			string text = mod.DisplayName;
 			this.modName = new UIText(text, 1f, false);
-			this.modName.Left.Set(modIconAdjust + 10f, 0f);
+			this.modName.Left.Set(10f, 0f);
 			this.modName.Top.Set(5f, 0f);
 			base.Append(this.modName);
 			UITextPanel<string> button = new UITextPanel<string>("Export", 1f, false);
@@ -48,21 +46,20 @@ namespace Localizer.UI
 			button.PaddingBottom -= 2f;
 			button.OnMouseOver += UICommon.FadedMouseOver;
 			button.OnMouseOut += UICommon.FadedMouseOut;
-			//button.OnClick += this.Moreinfo;
+			button.OnClick += ExportModText;
 			base.Append(button);
-			button2 = new UITextPanel<string>("...", 1f, false);
-			button2.Width.Set(100f, 0f);
-			button2.Height.Set(30f, 0f);
-			button2.Left.Set(button.Left.Pixels - button2.Width.Pixels - 5f, 0f);
-			button2.Top.Set(40f, 0f);
-			button2.PaddingTop -= 2f;
-			button2.PaddingBottom -= 2f;
-			button2.OnMouseOver += UICommon.FadedMouseOver;
-			button2.OnMouseOut += UICommon.FadedMouseOut;
+			//button2 = new UITextPanel<string>("...", 1f, false);
+			//button2.Width.Set(100f, 0f);
+			//button2.Height.Set(30f, 0f);
+			//button2.Left.Set(button.Left.Pixels - button2.Width.Pixels - 5f, 0f);
+			//button2.Top.Set(40f, 0f);
+			//button2.PaddingTop -= 2f;
+			//button2.PaddingBottom -= 2f;
+			//button2.OnMouseOver += UICommon.FadedMouseOver;
+			//button2.OnMouseOut += UICommon.FadedMouseOut;
 			//button2.OnClick += this.ToggleEnabled;
-			base.Append(button2);
+			//base.Append(button2);
 
-			Mod loadedMod = ModLoader.GetMod(mod.Name);
 			//if (loadedMod != null)
 			//{
 			//	loaded = true;
@@ -83,6 +80,19 @@ namespace Localizer.UI
 			//}
 		}
 
+		public void ExportModText(UIMouseEvent evt, UIElement listeningElement)
+		{
+			var path = Path.Combine(Main.SavePath, "ExportedText/", mod.Name);
+			if (!Directory.Exists(path))
+			{
+				Directory.CreateDirectory(path);
+			}
+			ExportTool.ExportItemTexts(mod, path);
+			ExportTool.ExportNPCTexts(mod, path);
+			ExportTool.ExportBuffTexts(mod, path);
+			ExportTool.ExportMiscTexts(mod, path);
+		}
+
 		public void DrawPanel(SpriteBatch spriteBatch, Vector2 position, float width)
 		{
 			spriteBatch.Draw(this.innerPanelTexture, position, new Rectangle?(new Rectangle(0, 0, 8, this.innerPanelTexture.Height)), Color.White);
@@ -94,10 +104,10 @@ namespace Localizer.UI
 		{
 			base.DrawSelf(spriteBatch);
 			CalculatedStyle innerDimensions = base.GetInnerDimensions();
-			Vector2 drawPos = new Vector2(innerDimensions.X + 5f + modIconAdjust, innerDimensions.Y + 30f);
-			spriteBatch.Draw(this.dividerTexture, drawPos, null, Color.White, 0f, Vector2.Zero, new Vector2((innerDimensions.Width - 10f - modIconAdjust) / 8f, 1f), SpriteEffects.None, 0f);
-			drawPos = new Vector2(innerDimensions.X + 10f + modIconAdjust, innerDimensions.Y + 45f);
-			this.DrawPanel(spriteBatch, drawPos, 85f);
+			Vector2 drawPos = new Vector2(innerDimensions.X + 5f, innerDimensions.Y + 30f);
+			spriteBatch.Draw(this.dividerTexture, drawPos, null, Color.White, 0f, Vector2.Zero, new Vector2((innerDimensions.Width - 10f) / 8f, 1f), SpriteEffects.None, 0f);
+			drawPos = new Vector2(innerDimensions.X + 10f, innerDimensions.Y + 45f);
+			//this.DrawPanel(spriteBatch, drawPos, 85f);
 		}
 
 		public override void MouseOver(UIMouseEvent evt)
