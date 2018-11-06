dbd1126ddf3b6eee9a729fb8d9cd354dbbff9cb0
AxeelAnder <axeel@qq.com>
2018-11-06 23:03:58 +0800

    add menu skeleton

--- Localizer/Interface.cs
+++ Localizer/Interface.cs
@@ -12,8 +12,9 @@ namespace Localizer
 {
 	public class Interface
 	{
-		internal const int BrowserID = 40000;
-		internal const int ManagerID = 40001;
+		internal const int MenuID = 50000;
+		internal const int BrowserID = 50001;
+		internal const int ManagerID = 50002;
 		internal static UIBrowser browser = new UIBrowser();
 		internal static UILocalManager manager = new UILocalManager();
 		
@@ -31,7 +32,34 @@ namespace Localizer
 		
 		public static bool LocalizerMenus(Main main, int selectedMenu, string[] buttonNames, float[] buttonScales, int[] buttonVerticalSpacing, ref int offY, ref int spacing, ref int numButtons, ref bool backButtonDown)
 		{
-			if (Main.menuMode == 100000)
+			if (Main.menuMode == MenuID)
+			{
+				offY = 210;
+				spacing = 42;
+				numButtons = 9;
+				buttonVerticalSpacing[numButtons - 1] = 18;
+				for (int i = 0; i < numButtons; i++)
+				{
+					buttonScales[i] = 0.75f;
+				}
+
+				int buttonIndex = 0;
+				buttonNames[buttonIndex] = "";
+				if (selectedMenu == buttonIndex)
+				{
+					Main.PlaySound(SoundID.MenuTick);
+				}
+
+				buttonIndex++;
+				buttonNames[buttonIndex] = Lang.menu[5].Value;
+				if (selectedMenu == buttonIndex || backButtonDown)
+				{
+					backButtonDown = false;
+					Main.menuMode = 11;
+					Main.PlaySound(11, -1, -1, 1);
+				}
+			}
+			if (Main.menuMode == ManagerID)
 			{
 				Localizer.TurnToManager();
 				return true;
